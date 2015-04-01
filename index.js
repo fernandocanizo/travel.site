da761dfd5e462b64d54df588fc079db8c85e782f
Fernando L. Canizo <flc@muriandre.com>
2015-04-01 10:57:01 -0300

    Implement logging. Ignore logs directory

--- index.js
+++ index.js
@@ -43,6 +43,18 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || tsDefaults.port);
 
+switch(app.get('env')) {
+	case 'development':
+		app.use(require('morgan')('dev'));
+		break;
+
+	case 'production':
+		app.use(require('express-logger')({
+			path: __dirname + 'log/requests.log'
+		}));
+		break;
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // middleware
