c5becbbef94ce2eaec0fe2f592b7fa991301ee5f
Fernando L. Canizo <flc@muriandre.com>
2015-05-20 11:46:34 -0300

    Bugfix, __dirname doesn't ends with slash, add it

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ switch(app.get('env')) {
 
 	case 'production':
 		app.use(require('express-logger')({
-			path: __dirname + 'log/requests.log'
+			path: __dirname + '/log/requests.log'
 		}));
 		break;
 }
